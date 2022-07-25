51baffcf32eba305fd6f20d94b7dfcecd6454c58
songing01 <79829662+songing01@users.noreply.github.com>
2022-07-25 12:03:40 +0200

    Feat: Let a frog move by pressing keyboard

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import time
+import keyboard
 
 tk =Tk()
 w=Canvas(tk,width=800,height=400)
@@ -15,6 +16,13 @@ while True:
   x+=1
   w.delete("all")
   w.create_rectangle(x,50,x+50,100)
+  if keyboard.is_pressed("up arrow"):
+      frog_y -=5
+  if keyboard.is_pressed("left arrow"):
+      frog_x -=5
+  if keyboard.is_pressed("right arrow"):
+      frog_x +=5
+
   w.create_rectangle(frog_x,frog_y,frog_x+frog_width,frog_y+frog_height,fill="green")
   w.update() #paints on the screen
   time.sleep(50/1000)
