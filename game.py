f4ee4fb9d027a3ac23ef15a256d9870da6a1b008
songing01 <79829662+songing01@users.noreply.github.com>
2022-07-25 11:46:37 +0200

    Feat: Create frog and car

--- game.py
+++ game.py
@@ -1,8 +1,20 @@
 from tkinter import *
+import time
 
 tk =Tk()
 w=Canvas(tk,width=800,height=400)
 w.pack()
-w.create_rectangle(50,50,100,100)
+
+x=50
+frog_x=400
+frog_y=300
+frog_width=30
+frog_height=30
+
 while True:
-    w.update()
+  x+=1
+  w.delete("all")
+  w.create_rectangle(x,50,x+50,100)
+  w.create_rectangle(frog_x,frog_y,frog_x+frog_width,frog_y+frog_height,fill="green")
+  w.update() #paints on the screen
+  time.sleep(50/1000)
