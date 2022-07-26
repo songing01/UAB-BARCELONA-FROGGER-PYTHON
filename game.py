cfc8096802ab1234c1ca29f1b975925aecde62e5
songing01 <79829662+songing01@users.noreply.github.com>
2022-07-26 10:23:42 +0200

    Feat: Create class for car and frog

--- game.py
+++ game.py
@@ -2,33 +2,69 @@ from tkinter import *
 import time
 import keyboard
 
-tk =Tk()
-w=Canvas(tk,width=800,height=400)
+
+class Frog:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+
+    def paint(self, w):
+        w.create_rectangle(self.x, self.y, self.x+self.width,
+                           self.y+self.height, fill="green")
+
+
+class Car:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+
+    def move(self):
+        self.x += 1
+
+    def paint(self, w):
+        w.create_rectangle(self.x, self.y, self.x +
+                           self.width, self.y+self.height)
+        w.create_line(self.x+self.width*0.75, self.y, self.x +
+                      self.width*0.75, self.y+self.height)
+
+
+tk = Tk()
+w = Canvas(tk, width=800, height=400)
 w.pack()
 
-x=50
-frog_x=400
-frog_y=300
-frog_width=30
-frog_height=30
+x = 50
+frog = Frog(400, 300, 30, 30)
+cars = [None]*3
+cars[0] = Car(50, 50, 50, 30)
+cars[1] = Car(50, 100, 50, 30)
+cars[2] = Car(50, 150, 50, 30)
 
 while True:
-  x+=1
-  w.delete("all")
-  w.create_rectangle(x,50,x+50,100)
+    for car in cars:
+        car.move()
 
-  if keyboard.is_pressed("up arrow"):
-      if frog_y-5>0:
-       frog_y -=5
-      else: print("Top reached")
-  if keyboard.is_pressed("left arrow"):
-      if frog_x -5>0:
-        frog_x -=5
-  if keyboard.is_pressed("right arrow"):
-      if frog_x+5<=770:
-        frog_x +=5
-  
+    w.delete("all")
 
-  w.create_rectangle(frog_x,frog_y,frog_x+frog_width,frog_y+frog_height,fill="green")
-  w.update() #paints on the screen
-  time.sleep(50/1000)
+    for car in cars:
+        car.paint(w)
+
+    if keyboard.is_pressed("up arrow"):
+        if frog.y-5 > 0:
+            frog.y -= 5
+        else:
+            print("Top reached")
+    if keyboard.is_pressed("left arrow"):
+        if frog.x - 5 > 0:
+            frog.x -= 5
+    if keyboard.is_pressed("right arrow"):
+        if frog.x+5 <= 770:
+            frog.x += 5
+
+    frog.paint(w)
+
+    w.update()  # paints on the screen
+    time.sleep(50/1000)
