0a99666b88b82dd1e1cdaf7dd7af60f98e553cdf
songing01 <79829662+songing01@users.noreply.github.com>
2022-07-26 10:33:29 +0200

    Feat: Set cars to show up again when they go out of screen

--- game.py
+++ game.py
@@ -25,6 +25,8 @@ class Car:
 
     def move(self):
         self.x += self.speed
+        if self.x > 800:
+            self.x = 50
 
     def paint(self, w):
         w.create_rectangle(self.x, self.y, self.x +
