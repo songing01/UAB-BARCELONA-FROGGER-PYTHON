2d616a0ab63c46c152322181115ab635b4265157
songing01 <79829662+songing01@users.noreply.github.com>
2022-07-27 10:23:45 +0200

    Feat: Set properties for each lane

--- game.py
+++ game.py
@@ -23,9 +23,12 @@ frog = Frog(400, 300, 30, 30)
 start_y = 50
 separator_y = 10
 lanes = [None]*3
+numOfCars = [3, 4, 5]
+speeds = [5, 4, 3]
+colors = ["red", "blue", "yellow"]
 for i in range(len(lanes)):
     lanes[i] = Lane(0, start_y+(50+separator_y) *
-                    i, 800, 70, 5, speed=5, color="red")
+                    i, 800, 70,  numOfCars[i], speed=speeds[i], color=colors[i])
 
 while True:
     for lane in lanes:
