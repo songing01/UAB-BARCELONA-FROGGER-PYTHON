341794e7046e7a24bdf4264dab28fa5abd0b32f9
songing01 <79829662+songing01@users.noreply.github.com>
2022-07-28 12:51:38 +0200

    Feat: Detect crashes

--- Frog.py
+++ Frog.py
@@ -4,7 +4,21 @@ class Frog:
         self.y = y
         self.width = w
         self.height = h
+    #self.img = PhotoImage(file="frog.jpg")
 
     def paint(self, w):
         w.create_rectangle(self.x, self.y, self.x+self.width,
                            self.y+self.height, fill="green")
+        #w.create_image(self.x,self.y, image=self.img)
+
+    def crashes(self, car):
+        if car.x <= self.x <= car.x+car.width and car.y <= self.y <= car.y+car.height:
+            return True
+        elif car.x <= self.x+self.width <= car.x+car.width and car.y <= self.y <= car.y+car.height:
+            return True
+        elif car.x <= self.x <= car.x and car.y <= self.y+self.height <= car.y+car.height:
+            return True
+        elif car.x <= self.x <= car.x+car.width and car.y <= self.y+self.height <= car.y+car.height:
+            return True
+        else:
+            return False
--- game.py
+++ game.py
@@ -1,18 +1,22 @@
 from tkinter import *
 import time
-from tkinter.ttk import Separator
 import keyboard
+
 from Car import *
 from Frog import *
 from Lane import *
 
+
+WIDTH = 800
+HEIGHT = 400
+
 tk = Tk()
-w = Canvas(tk, width=800, height=400)
+w = Canvas(tk, width=WIDTH, height=HEIGHT)
 w.pack()
 
 start_x = 50
 separator_x = 10
-frog = Frog(400, 300, 30, 30)
+frog = Frog(WIDTH/2, HEIGHT-100, 30, 30)
 
 start_y = 50
 separator_y = 20
@@ -23,22 +27,15 @@ colors = ["red", "blue", "yellow"]
 separator_x = [100, 70, 50]
 for i in range(len(lanes)):
     lanes[i] = Lane(0, start_y+(50+separator_y) *
-                    i, 800, 70,  numOfCars[i], speed=speeds[i], color=colors[i], separator_x=separator_x[i])
+                    i, WIDTH, 70,  numOfCars[i], speed=speeds[i], color=colors[i], separator_x=separator_x[i])
 
 t0 = time.time()
 isFinished = False
+
 while True:
     if not isFinished:
         for lane in lanes:
             lane.moveVehicles()
-
-        w.delete("all")
-        t = time.time()-t0
-        w.create_text(30, 20, text=str(int(t*100)/100),
-                      font=("bold", 15))
-
-        for lane in lanes:
-            lane.paint(w)
 
         if keyboard.is_pressed("up arrow"):
             if frog.y-5 > 0:
@@ -46,13 +43,8 @@ while True:
             else:
                 print("Top reached")
                 t = time.time()-t0
+                score = 100 - int(t*100)/100
                 isFinished = True
-                w.create_text(
-                    700, 30, text=f"record: {str(100 - int(t*100)/100)}", font=("bold", 20), fill="red")
-                # w.create_text(
-                #    400, 200, text="press 'enter' on your keboard to restart", font=("bold", 20))
-                # if keyboard.is_pressed("enter"):
-                #    isFinished = False
 
         if keyboard.is_pressed("left arrow"):
             if frog.x - 5 > 0:
@@ -60,8 +52,37 @@ while True:
         if keyboard.is_pressed("right arrow"):
             if frog.x+5 <= 770:
                 frog.x += 5
+    for lane in lanes:
+        for car in lane.cars:
+            if frog.crashes(car):
+                isFinished = True
+                score = 0
+
+    w.delete("all")
+
+    t = time.time()-t0
+
+    w.create_text(30, 20, text=str(int(t*100)/100),
+                  font=("bold", 15))
+    for lane in lanes:
+        lane.paint(w)
 
     frog.paint(w)
+    if isFinished:
+        if score > 0:
+            w.create_text(
+                700, 30, text=f"record: {str(score)}", font=("bold", 20), fill="red")
+        else:
+            w.create_text(
+                400, 200, text="press 'enter' on your keboard to restart", font=("bold", 20))
+            w.create_text(WIDTH/2, HEIGHT/2, fill="white",
+                          text=f"YOU LOOSE!!!!")
+
+    if keyboard.is_pressed("enter"):
+        print("pressed")
+        isFinished = False
+        w.update()
+        continue
 
     w.update()
     time.sleep(50/1000)
